1287ba793954657244229e3a98f256d1eead9117
Rafael <rafael2014barros@outlook.com>
2018-05-09 20:31:35 -0300

    Aula-08

---
+++ Aula-8/src/service/VendedorService.java
@@ -0,0 +1,21 @@
+package service;
+
+import java.util.ArrayList;
+
+import model.Pais;
+import dao.PaisDao;
+
+public class VendedorService {
+	PaisDao dao;
+	
+	public VendedorService(){
+		dao = new PaisDao();
+	}
+	public ArrayList<Pais> listarPais(){
+		return dao.listarPais();
+	}
+	public ArrayList<Pais> listarPais(String chave){
+		return dao.listarPais(chave);
+	}
+
+}
